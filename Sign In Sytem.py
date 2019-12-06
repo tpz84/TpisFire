2475941777f08944d60ce76e4bb3f03e0c2d6859
tpz84 <58535679+tpz84@users.noreply.github.com>
2019-12-05 17:35:35 -0700

    Add files via upload

---
+++ Sign In Sytem.py
@@ -0,0 +1,58 @@
+###############################################################################
+##This is used for signing in##
+import random 
+prime = 110339
+savedEmail = "liltp84@gmail.com"
+savedPassword = "Alpine84!"
+salt1 = "tnfccaujnk"
+salt2 = "niejmqfhep"
+
+def createSalt():
+    laps = 10
+    salt = ""
+    while laps > 0:
+        ranNum = random.randint(97,122)
+        ranLetter = chr(ranNum)
+        salt = salt + ranLetter
+        laps -= 1
+    return salt
+
+def hashThis(userCred):
+    hashedTerm = ""
+    toHash = salt1 + userCred + salt2
+    stringLength = len(toHash)
+    cutSpot = 0
+    while stringLength > 0: 
+        swapLetter = toHash[cutSpot]
+        numberV = ord(swapLetter)
+        hashedChar = numberV * prime
+        hashedTerm = int(str(hashedTerm) + str(hashedChar))
+        stringLength -= 1
+        cutSpot -= 1
+    return hashedTerm
+
+
+def login(savedEmail, savedPassword):
+    print("Welcome! Enter your email")
+    userEmail = input()
+    userEmail = userEmail.lower()
+    print("What is your password?")
+    userPassword = input()
+    hashedSavedEmail = hashThis(savedEmail)
+    hashedSavedPassword = hashThis(savedPassword)
+    hashedUserEmail = hashThis(userEmail)
+    hashedUserPassword = hashThis(userPassword)
+    if hashedSavedEmail == hashedUserEmail:
+        if hashedSavedPassword == hashedUserPassword:
+            print("Welcome")
+        else:
+            print("Incorrect Password")
+    else:
+        print("Provided email is incorrect")
+    
+
+
+    
+###############################################################################
+#main
+login(savedEmail, savedPassword)
